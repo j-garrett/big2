d9b21c574f52717a3353c2e969bb65392858549e
Jon Garrett <jonathan.garrett@gmail.com>
2016-11-02 13:23:34 -0700

    (lint)(refator)Change way server.js sets up socket

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 
-const path = require('path');
 const app = require('express')();
-const server = require('http').Server(app);
+const http = require('http');
+const path = require('path');
+
+const server = new http.Server(app);
 const io = require('socket.io')(server);
 
 const middleware = require('./config/middleware');
