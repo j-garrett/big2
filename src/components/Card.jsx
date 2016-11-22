526bda9c44725e5ca497cdfabdb9ce0b5a9be384
Jon Garrett <jonathan.garrett@gmail.com>
2016-11-22 09:52:38 -0800

    (feat)Add card images but not yet constrain their size

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,13 +5,17 @@ const Card = ({ value, onClick }) => (
     className={'card'}
     onClick={onClick}
   >
+    <img
+      src={`./images/cards/${value}.png`}
+      alt={value}
+    />
     {value}
   </li>
-)
+);
 
 Card.propTypes = {
   onClick: PropTypes.func,
   value: PropTypes.string.isRequired,
-}
+};
 
 export default Card;
