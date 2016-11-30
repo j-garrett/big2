c4e18993e4c60c28b44c452fe9f2c113fa5e12f5
Jon Garrett <jonathan.garrett@gmail.com>
2016-11-29 21:12:46 -0800

    (feat)Refine some of the logic functions to prep testing

--- server/config/computerPlayer.js
+++ server/config/computerPlayer.js
@@ -1,23 +1,25 @@
-const big2Rooms = require('./../models/big2Rooms');
+const rooms = require('./../models/big2Rooms').rooms;
 
-const suitValue = {
+const suitValueMap = {
   '♦': 1,
   '♣': 2,
   '♥': 3,
   '♠': 4,
 };
 
-const selectHand = () => {
+const selectHand = (user, room) => {
   // Return hand that is valid
+  const previousHand = rooms[room].pot.slice(-1, 1);
+  return previousHand;
 };
 
 const getValue = (card) => {
-  const value = parseInt(card.slice(0, card.length - 1));
+  const value = parseInt(card.slice(0, card.length - 1), 10);
   const suit = card[card.length - 1];
-  const suitValue = suitValue[suit];
+  const suitValue = suitValueMap[suit];
   return {
     value,
-    suitValue
+    suitValue,
   };
 };
 
@@ -25,38 +27,44 @@ const cardIsLarger = (card1, card2) => {
   const card1Val = getValue(card1);
   const card2Val = getValue(card2);
   return card1Val.value > card2Val.value
-        && card1Val.suitValue > card1Val.suitVale;
+        && card1Val.suitValueMap > card1Val.suitVale;
 };
 
 const suitIsMatch = (card1, card2) =>
-  getValue(card1).suitValue === getValue(card2).suitValue;
+  getValue(card1).suitValueMap === getValue(card2).suitValueMap;
 
 const valueIsMatch = (card1, card2) =>
   getValue(card1).value === getValue(card2).value;
 
 // only worry about singles, pairs, and trips for now
 // TODO: Make this function check for legal poker hands
-const handPatternCheck = hand => {
-
-}
-//   hand.reduce((acc, val) =>
-//     acc && valueIsMatch(acc, val) ? val : false,
-//     hand[0]);
-// }
+const handPatternCheck = (hand) => {
+  let result = true;
+  for (let i = 1; i < hand.length; i += 1) {
+    result = valueIsMatch(hand[i - 1], hand[i]) && result;
+  }
+  return result;
+};
 
 const handIsLarger = (hand, previousHand) =>
   hand.length === previousHand.length
-  && hand.reduce((acc, val, idx) =>
-    cardIsLarger(val, previousHand[idx]),
-    false);
+    && hand.reduce((acc, val, idx) =>
+      cardIsLarger(val, previousHand[idx]),
+      false);
 
 // TODO: Make sure hands are properly sorted for index compares
 const validCheck = (hand, previousHand) =>
   handPatternCheck(hand)
-      && handPatternCheck(previousHand)
-      && handIsLarger(hand, previousHand);
+    && handIsLarger(hand, previousHand);
 
 module.exports = {
+  suitValueMap,
   selectHand,
+  getValue,
+  cardIsLarger,
+  suitIsMatch,
+  valueIsMatch,
+  handPatternCheck,
+  handIsLarger,
   validCheck,
-}
+};
